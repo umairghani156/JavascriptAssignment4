10f6383f6f86c05a3dc1d078b71c146a3a21ba8a
umair <umairghani156@gamil.com>
2023-09-09 18:07:45 +0500

    JsAssignment 4

---
+++ app4.js
@@ -0,0 +1,27 @@
+ //first Recommendation 
+var myName="Umair", myAge="21years", myCourse="web development";
+// Five legal variables
+var myName;
+var my_name;
+var _firstname;
+var $name;
+var name1;
+//Five illegal variables
+ my name;
+ 156;
+ 9designation;
+ !myName;
+ user*name;
+
+// third recommendation
+var heading;
+    heading="<h1>Rules for naming JS variables</h1>";
+var _variable;
+    _variable="Variables names can only contain, numbers,$ and_.For example :<b>$my_1stVariable</b>";
+var variAble;
+    variAble="<br>Variables must begin with a letter, $ or _.For example:<b>$name</b>,<b>_name</b> or <b>name</b>";
+var variable3;
+    variable3="<br>Variable names are case sensitive";
+var $variable;
+    $variable="<br>Variables name should not be JS keywords";
+ document.write(heading,_variable,variAble,variable3,$variable);
